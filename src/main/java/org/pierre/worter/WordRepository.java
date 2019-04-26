f73099242a4da7a90b01b35dda0a9503540363e8
vernetto <publicpierre@gmail.com>
2019-04-26 02:08:35 +0200

    postcontruct load from file

--- src/main/java/org/pierre/worter/WordRepository.java
+++ src/main/java/org/pierre/worter/WordRepository.java
@@ -1,10 +1,16 @@
 package org.pierre.worter;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.pierre.worter.model.Genre;
 import org.pierre.worter.model.Type;
 import org.pierre.worter.model.Word;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -16,15 +22,19 @@ import static org.pierre.worter.model.Type.NOUN;
 @Service
 public class WordRepository {
 
-    List<Word> allNames = Arrays.asList(
-            Word.of("Löffel", M, NOUN),
-            Word.of("Gabel", F, NOUN),
-            Word.of("Messer", N, NOUN),
-            Word.of("Mensch", M, NOUN),
-            Word.of("Nachbarin", F, NOUN),
-            Word.of("Gast", M, NOUN)
-    )
-            ;
+    List<Word> allNames = new ArrayList<>();
+
+    @PostConstruct
+    void init() throws FileNotFoundException {
+        loadFromFile();
+    }
+
+    private void loadFromFile() throws FileNotFoundException {
+        FileReader reader = new FileReader("allwords.json");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        Word[] words = gson.fromJson(reader, Word[].class);
+        allNames = Arrays.asList(words);
+    }
 
     public List<Word> findAll() {
         return allNames;
